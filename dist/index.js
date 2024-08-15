48036adbccb471bd8c4cd427d7c4e77b63297089
Ayobami Ekundayo <ayobamieae@gmail.com>
2024-08-15 13:33:50 +0100

    Update build and start configurations

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const validation_pipe_1 = require("./common/pipes/validation.pipe");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.useGlobalPipes(new validation_pipe_1.ValidationPipe());
-    await app.listen(3001);
+    await app.listen(3000);
+    console.log('Application is running on: http://localhost:3001');
 }
 bootstrap();
