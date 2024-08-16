7ff6ba3ca0665be35538cb9b418bbd545fc803fc
Ayobami Ekundayo <ayobamieae@gmail.com>
2024-08-16 10:20:37 +0100

    Updated main.ts for dynamic port handling and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from './common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
